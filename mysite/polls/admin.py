e4e33e75a9b53205a08156dc2251312e9dd04d6f
gabarrameda <ga.barrameda@gmail.com>
2015-01-26 23:00:09 +0800

    editing parts of the page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from polls.models import Poll
 
-# Register your models here.
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,				{'fields':
+['question']}),
+		('Date information', {'fields':
+['pub_date'], 'classes': ['collapse']}),
+		]
+
+admin.site.register(Poll, PollAdmin)
