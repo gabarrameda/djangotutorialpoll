7678128cfe730a51e439637ce65d7e28f886f5ec
gabarrameda <ga.barrameda@gmail.com>
2015-01-27 21:52:44 +0800

    Half of part 3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^polls/', include('polls.urls')),
+)
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
