a1e6aae2916fdc5f2b94aa5207169953e4760d0f
gabarrameda <ga.barrameda@gmail.com>
2015-01-28 21:12:42 +0800

    Part 3 9:12PM

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
 	url(r'^polls/', include('polls.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 )
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
