6fa45471de23abbbf2b7c189a290f719287da194
gabarrameda <ga.barrameda@gmail.com>
2015-01-28 22:43:47 +0800

    Part 4 10:43 PM Editing Views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-from django.http import Http404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 
-from polls.models import Poll
+from polls.models import Choice, Poll
 
 # Create your views here.
 
@@ -21,3 +20,17 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % poll_id)
+
+def vote(reuest, poll_id):
+	p= get_object_or_404(Ppoll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+	else:
+		selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
