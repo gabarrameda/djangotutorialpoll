6a0c6ce53e6c139a6dbc5403fa6cef17ee1ac754
gabarrameda <ga.barrameda@gmail.com>
2015-01-28 22:06:34 +0800

    Part 3 10:06 PM 404 response

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def detail(request, poll_id):
 	poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+	return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
